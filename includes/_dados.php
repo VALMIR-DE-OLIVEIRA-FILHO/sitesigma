9d6539eb2ef7c5d969b714560eb698c95651b0a3
Valmir de Oliveira Filho <mirinho3789@gmail.com>
2022-06-02 12:27:19 -0300

    segundo-commit
    implementação de dados

---
+++ includes/_dados.php
@@ -0,0 +1,11 @@
+<?php
+$produtos= array(0=> array('nome'=>'capivara','preço'=>' 16000','descricao'=>' Uma simples capivara','imagem'=>'capivara.png')
+                 1=> array('nome'=>'leão','preço'=>' 20000','descricao'=>' Um simples leão','imagem'=>'leão.png')
+
+
+
+
+
+);
+
+?>
